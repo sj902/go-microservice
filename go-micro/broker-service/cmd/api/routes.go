6049616bfd263de8cbac75ffd44f5c49659aa9e6
sj902 <shubhamj@tuta.io>
2022-05-30 22:06:25 +0530

    Added makefiles(Ch-2)

--- go-micro/broker-service/cmd/api/routes.go
+++ go-micro/broker-service/cmd/api/routes.go
@@ -12,7 +12,7 @@ func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http ://*"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
